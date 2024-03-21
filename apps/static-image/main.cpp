b274bb00eb6353a0834b06e7fe6186b1ac116e7b
Ralph Ursprung <ralph.ursprung@gmail.com>
2024-03-21 17:29:52 +0100

    implement `static-image` app
    this adds a first version of the implementation which just reads a
    single image (path provided as an argument), processes that with the
    library and shows the annotated image at the end.
    
    this is very unsophisticaed and it does not yet show any analysis result
    (mainly because there are no results yet, `AnalysisResult` currently has
    no content).
    
    in a first step we can then just start printing the analysis result to
    the console (to provide additional information over what's already
    visible in the annotated image).
    a (much) fancier version of the application could provide its own UI
    with additional controls and embed the OpenCV image in there rather than
    letting OpenCV handle the window controls.
    
    note that we do not need to explicitly link OpenCV again in our app as
    we already link `banana-lib` which defines the OpenCV dependency as a
    public link, thus it's propagated to any target which links to it). the
    same is also the case for the include directories.
    
    this has been tested by temporarily not returning an error from
    `AnalyzeImage` (not commited as part of this commit). the application
    will work once the first step of the implementation has been done.
    
    closes #4

--- apps/static-image/main.cpp
+++ apps/static-image/main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <filesystem>
+#include <stdexcept>
+#include <format>
 
-int main() {
-    // TODO: implement static image feed (either single pictures and/or a saved video)
-    std::cerr << "not yet implemented!" << std::endl;
-    return 1;
+#include <opencv2/opencv.hpp>
+
+#include <banana-lib/lib.hpp>
+
+const cv::Size kWindowSize = {768, 512};
+
+[[nodiscard]]
+auto GetPathFromArgs(int const argc, char const * const argv[]) -> std::filesystem::path {
+    if(argc != 2) {
+        throw std::runtime_error(std::format("expected 1 argument but got {}!", argc-1));
+    }
+
+    auto const image_path = std::filesystem::path(argv[1]);
+    if (!std::filesystem::exists(image_path)) {
+        throw std::runtime_error(std::format("specified path does not exist: {}", image_path.string()));
+    }
+
+    return image_path;
+}
+
+void ShowAnalysisResult(banana::AnnotatedAnalysisResult const& analysis_result) {
+    std::string const windowName = "analysis result | press q to quit";
+    cv::namedWindow(windowName, cv::WINDOW_KEEPRATIO);
+    cv::imshow(windowName, analysis_result.annotated_image);
+    cv::resizeWindow(windowName, kWindowSize);
+    cv::waitKey();
+}
+
+int main(int const argc, char const * const argv[]) {
+    try {
+        auto const path = GetPathFromArgs(argc, argv);
+        auto const img = cv::imread(path.string());
+
+        auto const analysisResult = banana::AnalyzeAndAnnotateImage(img);
+
+        if(analysisResult) {
+            ShowAnalysisResult(analysisResult.value());
+        } else {
+            std::cerr << "failed to analyse the image: " << analysisResult.error().ToString() << std::endl;
+            return 1;
+        }
+    } catch (std::exception const& ex) {
+        std::cerr << ex.what() << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [image_path]" << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
