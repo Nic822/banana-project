55e309a5d44a7aed6583e950f8195afb653106d7
Ralph Ursprung <ralph.ursprung@gmail.com>
2024-03-21 18:01:44 +0100

    implement `livecam` app
    this is very similar to the `static-image` app.
    
    note that the name is not 100% correct, as it does not just allow using
    a live camera but it also allows using a video feed instead, as
    supported by OpenCVs `VideoCapture` API which offers both options (when
    passing an integer it is the index of the capture device, when passing a
    string it's a file path, URL or similar to be opened).
    
    just as with the `static-image` commit this has also been tested by
    temporarily not failing in the `AnalyzeImage` method and it also does
    not yet print any further results to the console (though printing to the
    console when processing a lot of frames per second is suboptimal).
    
    closes #5

--- apps/livecam/main.cpp
+++ apps/livecam/main.cpp
@@ -1,7 +1,65 @@
 #include <iostream>
+#include <filesystem>
+#include <stdexcept>
+#include <format>
 
-int main() {
-    // TODO: implement live camera feed
-    std::cerr << "not yet implemented!" << std::endl;
-    return 1;
+#include <opencv2/opencv.hpp>
+
+#include <banana-lib/lib.hpp>
+
+const cv::Size kWindowSize = {768, 512};
+
+[[nodiscard]]
+auto GetVideoCaptureFromArgs(int const argc, char const * const argv[]) -> cv::VideoCapture {
+    if(argc > 2) {
+        throw std::runtime_error(std::format("expected 0 or 1 arguments but got {}!", argc-1));
+    } else if(argc == 2) {
+        try {
+            // numeric value => it's the index of a video device
+            return cv::VideoCapture{std::stoi(argv[1])};
+        } catch (std::invalid_argument const& ex) {
+            // non-numeric value => treat it as a path, url or similar and let OpenCV check if it's valid
+            return cv::VideoCapture{argv[1]};
+        }
+    } else { // argc == 1
+        return cv::VideoCapture{0};
+    }
+}
+
+void ShowAnalysisResult(banana::AnnotatedAnalysisResult const& analysis_result) {
+    std::string const windowName = "analysis result | press q to quit";
+    cv::namedWindow(windowName, cv::WINDOW_KEEPRATIO);
+    cv::imshow(windowName, analysis_result.annotated_image);
+    cv::resizeWindow(windowName, kWindowSize);
+}
+
+int main(int const argc, char const * const argv[]) {
+    try {
+        auto cap = GetVideoCaptureFromArgs(argc, argv);
+        if(!cap.isOpened()) {
+            std::cerr << "can't use camera" << std::endl;
+            return 1;
+        }
+
+        while (true) {
+            cv::Mat frame;
+            cap >> frame;
+            auto const analysisResult = banana::AnalyzeAndAnnotateImage(frame);
+
+            if (analysisResult) {
+                ShowAnalysisResult(analysisResult.value());
+            } else {
+                std::cerr << "failed to analyse the image: " << analysisResult.error().ToString() << std::endl;
+                return 1;
+            }
+
+            if (cv::pollKey() == 'q') {
+                return 0;
+            }
+        }
+    } catch (std::exception const& ex) {
+        std::cerr << ex.what() << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [capture_device_id|video_path]" << std::endl;
+        return 1;
+    }
 }
